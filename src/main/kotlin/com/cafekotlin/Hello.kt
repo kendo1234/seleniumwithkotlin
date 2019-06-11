09dd19d984454a1880ef2c20555ea3526abbc829
TRT-KenTC <ken.talbot@raytheon.co.uk>
2019-06-11 14:39:29 +0100

    First Commit

---
+++ src/main/kotlin/com/cafekotlin/Hello.kt
@@ -0,0 +1,6 @@
+package com.cafekotlin
+
+fun main(args: Array<String>) {
+    println("Hello, World")
+}
+
---
+++ src/test/kotlin/com/cafekotlin/HelloTest.kt
@@ -0,0 +1,8 @@
+package com.cafekotlin
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class HelloTest {
+
+}
